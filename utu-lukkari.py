d32d9d1e7712803d5bf037cb377fd6b8d092253b
Miika Alikirri <miika.alik@gmail.com>
2020-09-02 18:53:51 +0300

    You can now select day in week view and it's opened in day view

--- utu-lukkari.py
+++ utu-lukkari.py
@@ -62,22 +62,74 @@ class DateDrawer:
     current_x = 0
     current_y = 0
 
+    draw_mode = None
+    draw_link_x = -1
+    draw_link_y = -1
+    draw_link_list = []
+
     def __init__(self):
         # Start curses mode
-        self.main_win = curses.initscr()
+        curses.initscr()
         # Line buffering disabled, Pass on everty thing to me
         curses.cbreak()
         # Don't show keypresses
         curses.noecho()
         # Hide cursor
         curses.curs_set(0)
-        # All those F and arrow keys
-        self.main_win.keypad(True)
+        # Make sure that color is working
+        curses.start_color()
+        # Link color
+        curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
         # The window we draw our calendar
         self.window = curses.newwin(80, 120, 1, 2)
+        # All those F and arrow keys
+        self.window.keypad(True)
 
     def destroy(self):
         curses.endwin()
+
+    def turn_highlight_on(self):
+        self.window.attron(curses.color_pair(1))
+        self.window.attron(curses.A_REVERSE)
+
+    def turn_highlight_off(self):
+        self.window.attroff(curses.color_pair(1))
+        self.window.attroff(curses.A_REVERSE)
+        pass
+
+    def reset_xy(self):
+        self.current_y = 0
+        self.current_x = 0
+
+    def reset_links(self):
+        self.draw_link_x = -1
+        self.draw_link_y = -1
+        self.draw_link_list = []
+
+    def handle_movement(self, c):
+        list_len = len(self.draw_link_list)
+        if list_len == 0:
+            return
+
+        day_str = None
+        if c == ord('l') or c == curses.KEY_RIGHT:
+            val = self.draw_link_x
+            self.draw_link_x = (val + 1) % list_len
+        elif c == ord('h') or c == curses.KEY_LEFT:
+            if self.draw_link_x == -1:
+                self.draw_link_x = list_len - 1
+            else:
+                val = self.draw_link_x
+                self.draw_link_x = (val + list_len - 1) % list_len
+        elif c == ord('\n'):
+            if self.draw_link_x != -1:
+                day_str = self.draw_link_list[self.draw_link_x]
+                self.draw_mode = "day"
+
+        if self.draw_mode == "week":
+            self.draw_week(False)
+        elif self.draw_mode == "day":
+            self.draw_day(day_str)
 
     def draw_loop(self):
         self.draw_day()
@@ -87,11 +139,16 @@ class DateDrawer:
                 self.destroy()
                 break
             elif c == ord('b'):
+                self.reset_links()
                 self.draw_day()
             elif c == ord('n'):
+                self.reset_links()
                 self.draw_week()
             elif c == ord('m'):
+                self.reset_links()
                 self.draw_month()
+            else:
+                self.handle_movement(c)
 
     def draw_string(self, string: str, max_len: int = -1):
         """Draw a string to the current x, y location"""
@@ -105,14 +162,15 @@ class DateDrawer:
     def draw_single_lecture(self):
         pass
 
-    def draw_day(self):
+    def draw_day(self, day_str: str = None):
+        self.draw_mode = "day"
         self.window.clear()
 
-        day_str = generate_dates("now")[0]
+        if not day_str:
+            day_str = generate_dates("now")[0]
         courses = course_wrap(day_str)
 
-        self.current_y = 0
-        self.current_x = 0
+        self.reset_xy()
         self.draw_string(day_str)
         self.current_y = 2
 
@@ -132,7 +190,8 @@ class DateDrawer:
 
         self.window.refresh()
 
-    def draw_week(self):
+    def draw_week(self, init: bool = True):
+        self.draw_mode = "week"
         self.window.clear()
 
         row_len = 20
@@ -141,16 +200,33 @@ class DateDrawer:
 
         # TODO: make dates selectable so we can show the single date info
         # TODO: make a single lecture selectable so we can show the full info
+        # TODO: show week column by column if the screen is too small
         # TODO: do we need to support weekends?
 
-        self.current_y = 0
-        self.current_x = 0
+        self.reset_xy()
         self.draw_string(f"{week_dates[0]} - {week_dates[4]}")
 
         for i, date in enumerate(week_dates[:5], 0):
             self.current_y = 2
             self.current_x = row_len * i
+            highlight = True
+
+            # Take the full date so we can load it later
+            if init:
+                self.draw_link_list.append(date)
+                highlight = False
+
+            if self.draw_link_x == -1:
+                highlight = False
+            elif self.draw_link_list[self.draw_link_x] != date:
+                highlight = False
+
+            if highlight:
+                self.turn_highlight_on()
             self.draw_string(f"  {date[:6]}")
+            if highlight:
+                self.turn_highlight_off()
+
             self.current_y = 4
             courses = course_wrap(date)
             for course in courses:
@@ -166,6 +242,7 @@ class DateDrawer:
         self.window.refresh()
 
     def draw_month(self):
+        self.draw_mode = "month"
         self.window.clear()
 
         row_len = 20
@@ -176,10 +253,10 @@ class DateDrawer:
         #       if the first day is not monday and/or last day is not friday-sunday
         # TODO: make dates selectable so we can show the single date info
         # TODO: make a single lecture selectable so we can show the full info
+        # TODO: show month column by column if the screen is too small
         # TODO: do we need to support weekends?
 
-        self.current_y = 0
-        self.current_x = 0
+        self.reset_xy()
         self.draw_string(
             f"{month_dates[0][0]} - {month_dates[len(month_dates) -1][0]}")
 
